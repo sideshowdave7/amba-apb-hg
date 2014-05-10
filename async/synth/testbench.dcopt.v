41d9dca554582cc207ca4bcce05d7792e14e53ac
David Hurst <sideshowdave7@gmail.com>
2014-05-09 23:02:47 -0600

    update with correct synthesis results

--- async/synth/testbench.dcopt.v
+++ async/synth/testbench.dcopt.v
@@ -3,10 +3,13 @@ module testbench ( PRESETn, TRANSFER, PREADY, PRDATA, PSEL, PENABLE );
   input [31:0] PRDATA;
   input PRESETn, TRANSFER, PREADY;
   output PSEL, PENABLE;
-  wire   n1;
-  assign PENABLE = 1'b0;
+  wire   zzz00, n2, n3, n4;
 
-  AO21_I U2 ( .A1(n1), .A2(PSEL), .B(TRANSFER), .Z(PSEL) );
-  INVERTBAL_E U5 ( .A(PREADY), .Z(n1) );
+  OA21_I U3 ( .A1(zzz00), .A2(TRANSFER), .B(PREADY), .Z(zzz00) );
+  AOI21_E U5 ( .A1(n2), .A2(PSEL), .B(zzz00), .Z(n3) );
+  INVERTBAL_E U8 ( .A(PREADY), .Z(n2) );
+  NAND2_A U9 ( .A(n3), .B(n4), .Z(PSEL) );
+  INVERTBAL_E U10 ( .A(TRANSFER), .Z(n4) );
+  NOR2_A U11 ( .A(n3), .B(TRANSFER), .Z(PENABLE) );
 endmodule
 
